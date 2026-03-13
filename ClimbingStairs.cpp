ce6a4461d15bc4cd0c821cd7a92030cea86b130b
Hrithik Sarma <91006716+Hrithiksarma@users.noreply.github.com>
2026-03-13 15:42:32 +0530

    Add files via upload

---
+++ ClimbingStairs.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+#include <vector>
+
+int count (int i, int n, vector <int> &dp) {
+if(i==n){
+return 1;
+}
+
+if(i>n){
+  return 0;
+}
+
+if(dp [i] != -1){
+  return dp[i];
+
+}
+
+return dp[i]= count(i+1,n,dp) + count(i+2,n,dp);
+
+}
+
+int main(){
+
+  int n=6;
+  int i=0; 
+  vector<int> dp(n+2,-1);
+  cout<<count(i,n,dp);
+
+
+
+
+
+}
+
