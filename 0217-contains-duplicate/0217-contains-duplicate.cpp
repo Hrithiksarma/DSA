740d6feace9e2eec99cb94575eede77daddf3c2f
Hrithik Sarma <91006716+Hrithiksarma@users.noreply.github.com>
2026-06-19 14:16:26 +0530

    Time: 51 ms (51.33%), Space: 90.8 MB (62.09%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        for(int i =1;i<nums.size();i++){
-            if(nums[i]==nums[i-1]){
+        unordered_set<int>hash;
+
+        for(int num:nums){
+            if(hash.count(num)==1){
                 return true;
             }
-            
+            else{
+                hash.insert(num);
+            }
+
         }
         return false;
-        
     }
 };
