f4a1eb1fdb50bc32e2be6433b7d869c4e960fc6a
Hrithik Sarma <91006716+Hrithiksarma@users.noreply.github.com>
2026-07-08 13:23:01 +0530

    Time: 2 ms (31.65%), Space: 20.8 MB (50.96%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        vector<vector<int>>matrixcopy=matrix;
-       int m = matrix.size();
-    
-        int n =matrix[0].size();
+        int m=matrix.size();
+        int n = matrix[0].size();
+        vector<bool>rowstozero(m,false);
+        vector<bool>colstozero(n,false);
 
-        for(int row=0;row<m;row++){
-            for (int col=0;col<n;col++){
-                if(matrixcopy[row][col]==0){
-                    for(int j=0;j<n;j++){
-                        matrix[row][j]=0;
-                    }
-                    for(int k=0;k<m;k++){
-                        matrix[k][col]=0;
-                    
-
+        for(int i=0;i<m;i++){
+            for(int j =0;j<n;j++){
+                if(matrix[i][j]==0){
+                    rowstozero[i]=1;
+                    colstozero[j]=1;
                 }
+
+
+            }
+        }
+        
+        for(int i =0; i<m;i++){
+            for(int j =0;j<n;j++){
+                if(rowstozero[i]==1||colstozero[j]==1){
+                    matrix[i][j]=0;
                 }
             }
         }
     }
-    
-
- 
-    
 };
