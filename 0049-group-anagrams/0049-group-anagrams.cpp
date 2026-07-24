f830db6a9e4475cfa2bcc603e55b035172bb6075
Hrithik Sarma <91006716+Hrithiksarma@users.noreply.github.com>
2026-07-24 23:26:57 +0530

    Time: 17 ms (53.39%), Space: 24.9 MB (80.08%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>>hash;
+
+        for(auto& word: strs){
+            string sorted_word= word;
+            sort(sorted_word.begin(),sorted_word.end());
+
+              hash[sorted_word].push_back(word);
+
+             
+             }
+
+           
+
+            vector<vector<string>>result;
+            for(auto & pair: hash){
+                result.push_back(pair.second);
+            }
+    return result;
+    }
+};
